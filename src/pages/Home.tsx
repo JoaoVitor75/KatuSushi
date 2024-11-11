2a080732740e7ebcba8c11659861496e9b3162d8
JoaoVitor75 <joaovitorferreira9843@gmail.com>
2024-11-11 19:16:45 -0300

    Footer
    Footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,6 +10,7 @@ import "../styles/tutorial.css";
 import "../styles/contact.css";
 import "../styles/testimonials.css";
 import "../styles/combos.css";
+import "../styles/footer.css";
 
 
 import HeroRectangleOne from "../assets/images/rectangleOne.png";
@@ -320,6 +321,44 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <footer className="footer">
+  <div className="container">
+    <div className="footer-content">
+      <div className="footer-logo">
+        <img src={Logo} alt="Katu Sushi" width={180} height={65} />
+        <p>Sushi feito com paixão e tradição desde 2024</p>
+      </div>
+      
+      <div className="footer-links">
+        <h4>Links Rápidos</h4>
+        <ul>
+          <li><a href="#hero">Home</a></li>
+          <li><a href="#tutorial">Como Pedir</a></li>
+          <li><a href="#combos">Combos</a></li>
+          <li><a href="#contact">Contato</a></li>
+        </ul>
+      </div>
+      
+      <div className="footer-contact">
+        <h4>Contato</h4>
+        <p>📱 (11) 99999-9999</p>
+        <p>📧 contato@katusushi.com.br</p>
+        <p>📍 Rua do Sushi, 123 - São Paulo</p>
+      </div>
+      
+      <div className="footer-hours">
+        <h4>Horário de Funcionamento</h4>
+        <p>Segunda a Sexta: 11h às 23h</p>
+        <p>Sábado e Domingo: 11h às 00h</p>
+      </div>
+    </div>
+    
+    <div className="footer-bottom">
+      <p>© 2024 Katu Sushi. Todos os direitos reservados.</p>
+    </div>
+  </div>
+</footer>
     </>
   );
 }
